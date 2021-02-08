57bccbddf5119e73d6010aa3cd76f21ed3ba164a
mareko87 <marekol87@gmail.com>
2021-02-08 13:41:20 +0100

    Stilizovan prikaz svih pojmova

--- src/components/Kreiraj.jsx
+++ src/components/Kreiraj.jsx
@@ -6,32 +6,42 @@ export default function Kreiraj(props) {
     return (
         <div className='container'>
             <div className='row mt-2'>
-                <div className='col-4'>
-                    <h2>Spisak pojmova</h2>
+                <div className='col-7'>
+                    <div className='row mt-2'>
+                        <div className='col-7'>
+                            <h2 >Spisak pojmova</h2>
+                        </div>
 
-                    <button onClick={(e) => {
-                        setPrikazi(prev => !prev)
-                    }} className='btn btn-primary'>{prikazi ? 'Sakrij' : 'Prikazi sve reci'}</button>
+                        <div className='col-4'>
+                            <button onClick={(e) => {
+                                setPrikazi(prev => !prev)
+                            }} className='btn btn-primary'>{prikazi ? 'Sakrij' : 'Prikazi '}</button>
 
-                    {
-                        prikazi && props.reci.length > 0 && (
-                            <ul>
-                                {props.reci.map((element, index) => {
-                                    return (
-                                        <li key={index}>
-                                            {element}
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        )
-                    }
+                        </div>
+                        {
+                            prikazi && props.reci.length > 0 && (
+                                <div className='row mt-2'>
 
+
+                                    <ul>
+                                        {props.reci.map((element, index) => {
+                                            return (
+                                                <li key={index}>
+                                                    {element}
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
+
+                            )
+                        }
+                    </div>
                 </div>
-                <div className='col-8'>
+                <div className='col-5'>
                     <form >
-                        <label>Rec</label>
-                        <input type="text" className='form-control' value={rec} onChange={(e) => {
+                        <label>Pojam</label>
+                        <textarea cols='10' rows='10' type="text" className='form-control' value={rec} onChange={(e) => {
                             const value = e.target.value;
 
                             setRec(value);
